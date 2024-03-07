591d8e8a0b8c57917d40f3dec584d90c20d79134
Gilson Silva <gilson.silva@globalhitss.com.br>
2024-03-07 16:04:07 -0300

    feat: add regras de validacao para dtos

--- src/main/java/br/com/ekan/api/domain/beneficiario/dto/DadosAtualizacaoBeneficiarioDto.java
+++ src/main/java/br/com/ekan/api/domain/beneficiario/dto/DadosAtualizacaoBeneficiarioDto.java
@@ -2,8 +2,9 @@ package br.com.ekan.api.domain.beneficiario.dto;
 
 import br.com.ekan.api.domain.beneficiario.Beneficiario;
 import br.com.ekan.api.domain.beneficiario.Documento;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,16 +19,18 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 public class DadosAtualizacaoBeneficiarioDto {
-    @NotNull
+    @NotBlank
     private Long id;
-    @NotNull
-    @NotEmpty
+
+    @NotBlank
     private String nome;
-    @NotNull
-    @NotEmpty
+
+    @Pattern(regexp = "^\\+?(?:55)?\\(?\\d{2}\\)?[-. ]?(?:\\d{4}|\\d{5})[-. ]?\\d{4}$", message = "O Telefone deve conter o código de area")
     private String telefone;
+
     @NotNull
     private LocalDate dataNascimento;
+
     private List<DadosAtualizacaoDocumentoDto> documentos;
 
     public List<Documento> converterDocumento() {
--- src/main/java/br/com/ekan/api/infrastructure/exception/RestExceptionHandler.java
+++ src/main/java/br/com/ekan/api/infrastructure/exception/RestExceptionHandler.java
@@ -1,17 +1,20 @@
 package br.com.ekan.api.infrastructure.exception;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler  {
 
     @ExceptionHandler(InvalidParameterException.class)
     public ResponseEntity<?> handleInvalidParameterException(InvalidParameterException exception,
@@ -34,12 +37,37 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 , null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(buildError(exception, HttpStatus.BAD_REQUEST, errors)
+                , null, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException exception,
+                                                     HttpServletRequest request) {
+        return new ResponseEntity<>(buildError(exception, HttpStatus.NOT_FOUND)
+                , null, HttpStatus.NOT_FOUND);
+    }
+
     private ErrorDetail buildError(Exception exception, HttpStatus status) {
         return new ErrorDetail.Builder()
                 .timeStamp(String.valueOf(new Date().getTime()))
                 .status(String.valueOf(status.value()))
                 .title("Error.")
                 .message(exception.getMessage())
+                .developerMessage(exception.getClass().getName())
+                .build();
+    }
+
+    private ErrorDetail buildError(Exception exception,  HttpStatus status, Map<String, String> errors) {
+        return new ErrorDetail.Builder()
+                .timeStamp(String.valueOf(new Date().getTime()))
+                .status(String.valueOf(status.value()))
+                .title("Error.")
+                .message(errors.toString())
                 .developerMessage(exception.getClass().getName())
                 .build();
     }
